920a1f9e2b7f6bac74f6d89fe3282aba6a09a36a
LucasB145 <99190894+LucasB145@users.noreply.github.com>
2022-06-15 12:00:37 +0200

    sortie form

--- src/Form/SortiesType.php
+++ src/Form/SortiesType.php
@@ -3,39 +3,45 @@
 namespace App\Form;
 
 use App\Entity\Lieux;
+use App\Entity\Participants;
+use App\Entity\Sites;
 use App\Entity\Sorties;
 use DateTime;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\DateTimeType;
 use Symfony\Component\Form\Extension\Core\Type\DateType;
+use Symfony\Component\Form\Extension\Core\Type\NumberType;
 use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Validator\Constraints\Date;
 
 class SortiesType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('nom')//changer les titres + mettre les calendriers
-            ->add('datedebut', DateType::class)
-            ->add('duree')
-            ->add('datecloture',DateType::class)
-            ->add('nbinscriptionsmax')
-            ->add('descriptioninfos', TextareaType::class)
+            ->add('nom', TextType::class, ["label" => "Nom"]) //changer les titres + mettre les calendriers
+            ->add('datedebut', DateType::class, ["label" => "Date de début", "widget" => "single_text"])
+            ->add('duree', NumberType::class, ["label" => "Durée"])
+            ->add('datecloture', DateType::class, ["label" => "Date de cloture des inscriptions", "widget" => "single_text"])
+            ->add('nbinscriptionsmax', NumberType::class, ["label" => "Nombre maximum d'inscriptions"])
+            ->add('descriptioninfos', TextareaType::class, ["label" => "Description de l'activité"])
             ->add('etatsortie')
             ->add('urlphoto')
-            // ->add('sitesNoSite')
+            // ->add('sitesNoSite', EntityType::class,["class"=>Sites::class])
             // ->add('etatsNoEtat')
             ->add('organisateur')
-            ->add('lieuxNoLieu', EntityType::class,[
-                "class"=>Lieux::class,
-                "choice_label"=>function ($lieuxNoLieu){
+            ->add('lieuxNoLieu', EntityType::class, [
+                "label" =>"Lieu de la sortie",
+                "class" => Lieux::class,
+                "choice_label" => function ($lieuxNoLieu) {
                     return $lieuxNoLieu->getVillesNoVille()->getNomVille();
                 }
             ])
-            // ->add('participantsNoParticipant')
+            // ->add('participantsNoParticipant',EntityType::class,["class"=>Participants::class])
         ;
     }
 
